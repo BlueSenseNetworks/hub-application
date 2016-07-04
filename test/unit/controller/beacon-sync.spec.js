b4d44a9f863e6b72d48e2aa23ff6360a4a0bbd2b
Vladimir Petrov <vlad@bluesensenetworks.com>
2016-07-04 14:56:56 +0100

    SyncBeacon message handling interim implementation

---
+++ test/unit/controller/beacon-sync.spec.js
@@ -0,0 +1,62 @@
+const BleScanner = require('../../../lib/bluesense-superhub/ble-scanner');
+const Bus = require('../../../lib/bluesense-superhub/messaging/bus');
+const Message = require('../../../lib/bluesense-superhub/models/message');
+const InfoMessage = require('../../../lib/bluesense-superhub/models/messages/info');
+const Logger = require('../../../lib/bluesense-superhub/logger');
+const os = require('os');
+const fs = require('fs');
+const Machine = require('../../../lib/bluesense-superhub/machine');
+
+describe('BeaconSync', function() {
+  before(function() {
+  });
+
+  beforeEach(function() {
+    this.sandbox = sinon.sandbox.create();
+
+    this.bleScannerMock = this.sandbox.mock(BleScanner.prototype);
+    this.busMock = this.sandbox.mock(Bus.prototype);
+    this.loggerStub = sinon.createStubInstance(Logger);
+    this.osMock = this.sandbox.mock(os);
+    this.machineMock = this.sandbox.mock(Machine);
+
+    this.BeaconSync = proxyquire('../../lib/bluesense-superhub/controller/beacon-sync', {
+      '../messaging/bus': {
+        create: () => this.busMock.object
+      },
+      './ble-scanner': {
+        create: () => this.bleScannerMock.object
+      },
+      '../logger': {
+        getInstance: () => this.loggerStub
+      },
+      'os': this.osMock.object,
+      '../machine': this.machineMock.object
+    });
+
+    this.BeaconSync.create();
+  });
+
+  afterEach(function() {
+    this.busMock.object.removeAllListeners();
+
+    this.sandbox.restore();
+  });
+
+  describe('message broker event handling', function() {
+    context(Message.type.syncBeacon, function() {
+      it('should start syncing a beacon config on BeaconSync message', function() {
+
+        let message = {
+          serialNumber: '123456789'
+        };
+
+        this.busMock.expects('publish').withArgs(message);
+
+        this.busMock.object.emit(Message.type.syncBeacon, message);
+
+        this.busMock.verify();
+      });
+    });
+  });
+});
